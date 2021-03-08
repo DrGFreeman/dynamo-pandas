f7a4ffbbd75ad2905031e60a9f74976d404d535e
DrGFreeman <DrGFreeman@users.noreply.github.com>
2021-03-08 08:24:24 -0500

    Read version without importing
    Refactor setup.py to read the package version from its init file without
    importing it.
    
    Update interrogate configuration to exclude setup.py from verification.

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
+import codecs
+from pathlib import Path
+import re
+
 from setuptools import find_packages
 from setuptools import setup
 
-from dynamo_pandas import __version__
+
+def read(filename):
+    file_path = Path(__file__).parent / filename
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+def find_meta(meta):
+    """Extract __*meta*__ from META_FILE."""
+    re_str = r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta)
+    meta_match = re.search(re_str, META_FILE, re.M)
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError(f"Unable to find __{meta}__ string.")
+
+
+PACKAGE_NAME = "dynamo-pandas"
+META_FILE = read(Path(__file__).parent / PACKAGE_NAME.replace("-", "_") / "__init__.py")
+
 
 setup(
-    name="dynamo-pandas",
-    version=__version__,
+    name=PACKAGE_NAME,
+    version=find_meta("version"),
     author="Julien de la Bruère-Terreault",
     author_email="drgfreeman@tuta.io",
     maintainer="Julien de la Bruère-Terreault",
