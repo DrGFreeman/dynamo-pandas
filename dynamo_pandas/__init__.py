121f4afb4225d0fb634e1da631d132d58ead5424
DrGFreeman <DrGFreeman@users.noreply.github.com>
2021-04-25 09:40:27 -0400

    Set version to 1.2.1

--- dynamo_pandas/__init__.py
+++ dynamo_pandas/__init__.py
@@ -2,6 +2,6 @@ from .dynamo_pandas import get_df
 from .dynamo_pandas import keys
 from .dynamo_pandas import put_df
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 __all__ = ["get_df", "keys", "put_df", "__version__"]
